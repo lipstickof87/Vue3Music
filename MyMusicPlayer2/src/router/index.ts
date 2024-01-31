0a157fc82e0b2a1481117f5a7fd1d81d66645dfc
lipstickof87 <2380588373@qq.com>
2024-01-31 15:55:07 +0800

    导入静态页面

--- MyMusicPlayer2/src/router/index.ts
+++ MyMusicPlayer2/src/router/index.ts
@@ -1,23 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+/**
+  ts版本的路由
 
+ */
+
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+  RouteRecordRaw
+} from 'vue-router'
+
+import Main from '../components/Main.vue'
+
+/**
+ * 定义路由模
+ */
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: 'main'
+  },
+  {
+    path: '/main',
+    name: 'Main',
+    component: Main,
+    redirect: '/ContainerMainFind',
+    
+    
+  }
+]
+
+/**
+ * 定义返回模块
+ */
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHashHistory(import.meta.env.BASE_URL), // 设置为history模式，就是路径里面没有#,  createWebHashHistory 是默认的，括号里面的就是基础路径，可以理解为项目名称，就是请求路径的基础url
+  routes
 })
 
 export default router
